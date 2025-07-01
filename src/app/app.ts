9ba6a4aabf30ecd2639189a43ee056e24be8cac1
Andy-mini <lining.arthur@gmail.com>
2025-07-01 22:12:06 +0200

    refactoring

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,7 @@ import {registerRoutes} from "./routes";
 export const app = Fastify({ logger: true });
 
 // Serve static files
+// find the "redoc.standalone.js" and place it to "/assets/redoc.standalone.js"
 app.register(fastifyStatic, {
     root: path.join(__dirname, '../../node_modules/redoc/bundles'),
     prefix: '/assets/',
